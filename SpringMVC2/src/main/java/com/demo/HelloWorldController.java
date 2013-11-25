25bae638444163aeb7133718ec3936730046bf36
OlgaE <olga.efimova.aol@gmail.com>
2013-11-25 22:46:57 +0700

    Initial commit (gives an error: No mapping found for HTTP request with URI [/SpringMVC2/welcome.htm] in DispatcherServlet with name 'spring-mvc').

---
+++ SpringMVC2/src/main/java/com/demo/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloWorldController extends AbstractController {
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+		return new ModelAndView("hellopage", "key", message);
+	}
+
+}
